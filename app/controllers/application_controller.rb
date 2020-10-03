ebf39bcf0a778411c847b4a0c5a59546132449c0
mayu-nakai <70678306+mayu-nakai@users.noreply.github.com>
2020-10-04 07:29:32 +0900

    トップページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name,:first_name,:kanalast_name,:kanafirst_name,:birthday])
+  end
 
 
   private
