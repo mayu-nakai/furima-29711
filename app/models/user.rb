914046add28283bc2a74886a8145e0bf337df4c2
mayu-nakai <70678306+mayu-nakai@users.noreply.github.com>
2020-10-06 07:26:52 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ApplicationRecord
   validates :nickname, presence: true
+
+  has_many :items
+  has_many :orders
+
+  with_options presence: true do
+    validates :nickname, :birthday, presence: true
+    validates :family_name, :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :family_name_kana, :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ } 
+  end
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
