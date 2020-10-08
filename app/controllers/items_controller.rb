f4393cfe3ccfbc5bc5cde8146889dd92a496e656
mayu-nakai <70678306+mayu-nakai@users.noreply.github.com>
2020-10-09 05:04:45 +0900

    ActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+  
+  private
+    def item_params
+      params.require(:item).permit(:image, :name, :description, :price, :category_id, :condition_id, :delivery_charge_id, :delivery_day_id, :prefecture_id, :user)
+    end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,33 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivery_day
+
+  belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :delivery_day_id
+    validates :image
+    validates :price
+  end
+
+  validates_inclusion_of :price, in: (300..9_999_999), format: { with: /^[0-9]+$/ }
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :delivery_day_id
+  end
 end
