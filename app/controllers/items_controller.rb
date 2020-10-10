8c3f99fc7307107347c04fa53e06da24bbb449af
mayu-nakai <70678306+mayu-nakai@users.noreply.github.com>
2020-10-11 06:20:50 +0900

    一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,16 @@
 class ItemsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
     @item = Item.new
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def create
