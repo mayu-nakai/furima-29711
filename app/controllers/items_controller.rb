9570af570be6f06a5d350f67f34b472265a4d3db
mayu-nakai <70678306+mayu-nakai@users.noreply.github.com>
2020-10-12 08:27:52 +0900

    編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -9,7 +11,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
   end
 
   def create
@@ -21,8 +25,20 @@ class ItemsController < ApplicationController
     end
   end
   
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
     def item_params
       params.require(:item).permit(:image, :name, :description, :price, :category_id, :condition_id, :delivery_charge_id, :delivery_day_id, :prefecture_id).merge(user_id: current_user.id)
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
     end
 end
